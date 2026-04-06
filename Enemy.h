be4cd4bf0df34cea804d1a0d66b714f0857f4223
dungtrantrung2701 <dungtrantrung2701@gmail.com>
2026-04-06 16:09:00 +0700

    Add files via upload

---
+++ Enemy.h
@@ -0,0 +1,28 @@
+#ifndef ENEMY_H_INCLUDED
+#define ENEMY_H_INCLUDED
+
+#include <SDL2/SDL.h>
+
+struct Enemy {
+    float x, y;
+    float speed;
+git
+    int health;
+    bool isAlive;
+
+    int type;
+    float originX;
+    float amplitude;
+    float frequency;
+
+    SDL_Texture* texture;
+};
+
+Enemy* createEnemy(float x, float y, int type, SDL_Texture* tex);
+void updateEnemy(Enemy* e);
+void renderEnemy(SDL_Renderer* renderer, Enemy* e);
+
+#endif
+
+
+#endif // ENEMY_H_INCLUDED
